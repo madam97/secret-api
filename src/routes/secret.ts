dfb7be4ef369de521f6bb20a9d44ef514ebde4ef
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-06 18:39:53 +0200

    added secret POST endpoint

--- src/routes/secret.ts
+++ src/routes/secret.ts
@@ -8,7 +8,30 @@ router.get('/:hash', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  res.send('TODO');
+  try { 
+    const now = Date.now();
+
+    const secret = new Secret({
+      hash: req.body.secretText + now,
+      secretText: req.body.secretText,
+      createdAt: now,
+      expiresAt: new Date( now + parseInt(req.body.expiresAt) * 1000 )
+    });
+  
+    const data = await secret.save();
+    
+    res.status(200).json({
+      hash: data.hash,
+      secretText: data.secretText,
+      createdAt: data.createdAt,
+      expiresAt: data.expiresAt
+    });
+  } catch (err) {
+    res.status(405).json({
+      msg: 'Invalid input'
+    });
+  }
+
 });
 
 export default router;
