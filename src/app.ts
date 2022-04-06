f49d4de9e053451f929a515068ed5da246d50cb5
Mézes Ádám <mezesadam1997@gmail.com>
2022-04-06 20:57:33 +0200

    moved app init to app.ts file, import it in index.ts

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import secretRoute from './routes/secret';
+
+// Config
+dotenv.config({ path: `.env${process.env.NODE_ENV ? '.'+process.env.NODE_ENV : ''}` });
+
+// App
+const app = express();
+
+// Init middleware
+app.use(express.json());
+
+app.use('/secret', secretRoute);
+
+// Connect to DB
+mongoose.connect(
+  process.env.DB_CONNECTION ?? '', 
+  () => console.log('Connected to database')
+);
+
+export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,5 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import secretRoute from './routes/secret';
+import app from './app';
 
-// Config
-dotenv.config({ path: `.env${process.env.NODE_ENV ? '.'+process.env.NODE_ENV : ''}` });
-
-// App
-const app = express();
-
-// Init middleware
-app.use(express.json());
-
-app.use('/secret', secretRoute);
-
-// Connect to DB
-mongoose.connect(
-  process.env.DB_CONNECTION ?? '', 
-  () => console.log('Connected to database')
-);
-
-// Start server
 app.listen(
   process.env.PORT, 
   () => console.log(`Server started on port ${process.env.PORT}`)
